ff9115fb8641689801c1fa07d794e98714bc2242
Berkay Yaman <berkayyaman95@gmail.com>
2018-08-02 10:28:55 +0300

    Update ALU
    Overflow and carry fixed.

--- ALU/alu.v
+++ ALU/alu.v
@@ -5,7 +5,7 @@ input [2:0] ALU_Sel;
 output [7:0] ALU_Out;
 output carry,zero,neg,over;
 
-wire  [8:0] tmp ;
+  wire  [8:0] tmp;
 
 wire [7:0] Result000,Result001,Result010,Result011,Result100,Result101,Result110,Result111;
 
@@ -20,13 +20,16 @@ assign Result101 = A<<1;
 
 assign Result110 = A>>1;
 assign Result111 = 0;
+  
 
 mux8to1 mux_out(Result000,Result001,Result010,Result011,Result100,Result101,Result110,Result111,ALU_Sel[0],ALU_Sel[1],ALU_Sel[2],ALU_Out);
-assign tmp = A+B;
+  
+  mux2to1_9bit mux_carry({9{0},A+B},{9{0},A-B},ALU_Sel[2],tmp);
+
 assign carry = tmp[8];
 assign zero = ~(ALU_Out[7] | ALU_Out[6] | ALU_Out[5] | ALU_Out[4] | ALU_Out[3] | ALU_Out[2] | ALU_Out[1] | ALU_Out[0]);
 assign neg = ALU_Out[7] ;
-assign over = (A[7] & B[7] & ~ALU_Out[7]) | (~A[7] & ~B[7] & ALU_Out[7]);
+  assign over = (A[7] & B[7] & ~ALU_Out[7]) | (~A[7] & ~B[7] & ALU_Out[7]) | tmp[8];
 
 
 endmodule
