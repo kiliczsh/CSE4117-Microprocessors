de9bc4eb1aa4e75a7d102d51751956cc2293b589
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2018-08-03 19:36:14 +0300

    added sample rom

---
+++ ROM/rom.v
@@ -0,0 +1,21 @@
+module rom(addr,clk ,output); 
+output[15:0] output;
+input[2:0] addr;
+input clk;
+reg [15:0] output;
+reg [15:0] ROM[15:0];
+
+always @(negedge clk)
+begin
+ROM[0]=16'h5601; ROM[1]=16'h3401;
+ROM[2]=16'h1801; ROM[3]=16'h0ac1;
+ROM[4]=16'h0521; ROM[5]=16'h0221;
+ROM[6]=16'h5601; ROM[7]=16'h5401;
+ROM[8]=16'h4801; ROM[9]=16'h3801;
+ROM[10]=16'h3001; ROM[11]=16'h2401;
+ROM[12]=16'h1c01; ROM[13]=16'h1601;
+ROM[14]=16'h5601; ROM[15]=16'h5401;
+out=ROM[addr];
+end
+
+endmodule
