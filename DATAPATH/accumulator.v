848b3d718f40165e256341e35910671bd7ab69b2
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2018-08-05 18:34:00 +0300

    Update accumulator.v

--- DATAPATH/accumulator.v
+++ DATAPATH/accumulator.v
@@ -2,12 +2,12 @@ module accumulator (in, acc, clk, reset);
 
 input [7:0] in;
 input clk, reset;
-output [7:0] acc
+  output [7:0] acc;
 reg [7:0] acc;
 
 always@(clk) begin
 if(reset)
-acc <= 8’b00000000;
+acc <= 0;
 else
 acc <= in;
 end
