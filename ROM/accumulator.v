fe719566f2aad1887eacfa498c089383aeaa9f42
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2018-08-05 16:42:34 +0300

    Create accumulator.v

---
+++ ROM/accumulator.v
@@ -0,0 +1,14 @@
+module accumulator (in, acc, clk, reset);
+
+input [7:0] in;
+input clk, reset;
+output [7:0] acc
+reg [7:0] acc;
+
+always@(clk) begin
+if(reset)
+acc <= 8’b00000000;
+else
+acc <= acc + in;
+end
+endmodule
