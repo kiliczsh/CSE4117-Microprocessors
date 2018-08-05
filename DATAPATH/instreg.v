4df75823640281152e04b0b7c433076e63dae73c
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2018-08-05 18:49:38 +0300

    Update instreg.v

--- DATAPATH/instreg.v
+++ DATAPATH/instreg.v
@@ -1,11 +1,12 @@
 module instreg(inst,clk,A,B,ALU_Sel);
-  input[7:0] inst;
-  input clk;
-    output[7:0] A,B;
-  output[2:0] ALU_Sel;
-  
-  
-  
-  
-  
- endmodule
+input[7:0] inst;
+input clk;
+output[7:0] A,B;
+output[2:0] ALU_Sel;  
+always @ (posedge clk)
+begin
+  assign ALU_Sel[0] = inst[0];
+  assign ALU_Sel[1] = inst[1];
+  assign ALU_Sel[2] = inst[2];
+end
+endmodule
