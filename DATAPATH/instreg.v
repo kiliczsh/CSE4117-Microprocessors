1ff6407f051b98948d615f7d27e3127b74bf09de
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2018-08-08 09:45:07 +0300

    added instruction set architechture

--- DATAPATH/instreg.v
+++ DATAPATH/instreg.v
@@ -1,12 +1,21 @@
-module instreg(inst,clk,A,B,ALU_Sel);
-input[7:0] inst;
+module instreg(inst,clk,A,ALU_Sel);
+input[10:0] inst;
 input clk;
-output[7:0] A,B;
-output[2:0] ALU_Sel;  
+output[7:0] A;
+output[2:0] ALU_Sel;
+  
 always @ (posedge clk)
 begin
   ALU_Sel[0] <= inst[0];
   ALU_Sel[1] <= inst[1];
   ALU_Sel[2] <= inst[2];
+  A[0] <= inst[3];
+  A[1] <= inst[4];
+  A[2] <= inst[5];
+  A[3] <= inst[6];
+  A[4] <= inst[7];
+  A[5] <= inst[8];
+  A[6] <= inst[9];
+  A[7] <= inst[10];
 end
 endmodule
