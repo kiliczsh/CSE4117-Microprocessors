14d40b26675baa95fd239f9c9dc9f714da30a6a7
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2018-08-08 10:25:58 +0300

    Update rom.v

--- DATAPATH/rom.v
+++ DATAPATH/rom.v
@@ -6,10 +6,14 @@ reg [15:0] out;
 reg [15:0] ROM[7:0];
 
 initial begin
-    ROM[0]=16'h5601; ROM[1]=16'h3401;
-    ROM[2]=16'h1801; ROM[3]=16'h0ac1;
-    ROM[4]=16'h0521; ROM[5]=16'h0221;
-    ROM[6]=16'h5601; ROM[7]=16'h5401;
+    ROM[0]=16'h0400; //MOV 0
+    ROM[1]=16'h0070; //ADD 112
+    ROM[2]=16'h010C; //SUB 12
+    ROM[3]=16'h0502; //<<2
+    ROM[4]=16'h0400; //MOV 0
+    ROM[5]=16'h0080; //ADD 128
+    ROM[6]=16'h0080; //ADD 128
+    ROM[7]=16'h0400; //MOV 0 
 end
 
 always @(negedge clk)
